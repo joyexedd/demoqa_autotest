0b2aa13b3b2b2c9f86b901d8c4052f5ba3a0875d
joyexedd <joyexedd@gmail.com>
2022-05-17 15:21:36 +0300

    add changes

---
+++ src/test/java/guru/qa/SimpleTest.java
@@ -0,0 +1,24 @@
+package guru.qa;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.*;
+
+public class SimpleTest {
+    @BeforeEach
+    void openYa() {
+        Selenide.open("https://ya.ru");
+    }
+    @AfterEach
+    void closeYa() {
+        Selenide.closeWindow();
+    }
+
+    @Test
+    void assertTest() {
+        Assertions.assertTrue(2>3);
+    }
+    @Test
+    void assertTest2() {
+        Assertions.assertTrue(2<3);
+    }
+}
