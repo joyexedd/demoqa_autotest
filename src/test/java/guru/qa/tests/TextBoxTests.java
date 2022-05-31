eb3ca9eaf2aaa51401a8d9bf0c73ebed77ed6a29
joyexedd <joyexedd@gmail.com>
2022-05-31 14:06:52 +0300

    add changes

--- src/test/java/guru/qa/tests/TextBoxTests.java
+++ src/test/java/guru/qa/tests/TextBoxTests.java
@@ -3,12 +3,17 @@ package guru.qa.tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import pages.components.calendarComponents;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class TextBoxTests {
 
+public class TextBoxTests {
+String day = "01";
+String month = "July";
+String year = "1989";
     @BeforeAll
     static void SetUp(){
     Configuration.holdBrowserOpen = true;
@@ -20,10 +25,23 @@ public class TextBoxTests {
     @Test
     void fillFormTest() {
         open("/text-box");
-        $("[id=userName]").setValue("Yago Yago");
-        $("[id=currentAddress]").setValue("Moscow, MID, 12 floor");
-        $("[id=userEmail]").setValue("Yago@Yago.ru");
-        $("[id=permanentAddress]").setValue("Same");
-        $("[id=submit]").click();
+        $("[#userName]").setValue("Yago Yago");
+        $("[#currentAddress]").setValue("Moscow, MID, 12 floor");
+        $("[#userEmail]").setValue("Yago@Yago.ru");
+        $("[#permanentAddress]").setValue("Same");
+        $("[#submit]").click();
+    }
+
+    @Test
+    void registrationFormTest(){
+        open("/automation-practice-form");
+        $("[#firstName]").setValue("Vasily");
+        $("[#lastName]").setValue("Ivanov");
+        $("[#userEmail]").setValue("vassyaivanoff");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber-wrapper").$("[#userNumber]").setValue("8889999389");
+        $("#dateOfBirthInut").click();calendarComponents.setDate(day,month,year);
+        $("[#userNumber]").setValue("8999994949494");
+
     }
 }
